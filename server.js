96fa8ffd9604b2b84b0036f05b1f28ef691fcf19
mojoboss <mojo.killer1@gmail.com>
2016-04-10 00:19:36 +0530

    api complete

--- server.js
+++ server.js
@@ -10,13 +10,21 @@ var jobsJson = require(filename);  //jobsJson is a json object
 //create express app
 var app = express();
 app.get("/:jobtitle", function(req, res){         
-	console.log(req.params.jobtitle);
+	var job = req.params.jobtitle;    //extract the jobTitle passed as a string parameter
+	//console.log(job);
+	var maxx = 0, ind=0;
 	for(var i = 0; i < jobsJson.length; i++) {
-		//var obj = jobsJson[i];
-		//console.log(obj.role);
-		//res.write(obj.role + "\t" + obj.skills + "\n");
-
+		var obj = jobsJson[i];
+		var tempJob = obj.role;
+		var diceCoeff = stringSimilarity.compareTwoStrings(job, tempJob);   //compute string similarity based on Dice's Coefficient
+		if(diceCoeff >= maxx){
+			maxx = diceCoeff;
+			ind = i;
+		}
 	}
+	//console.log(jobsJson[ind].skills);
+	res.send(jobsJson[ind].skills);
+	
 	
 });
 
